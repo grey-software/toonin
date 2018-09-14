0f456d5c5b5d6ed2d3bd576ef1b2cad3b6317c6b
arsalabangash <arsalabangash@gmail.com>
2018-09-14 01:33:43 +0000

    Added gitignore and got this working again

---
+++ app/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const path = require('path');
+const app = express();
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/ping', function (req, res) {
+ return res.send('pong');
+});
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(process.env.PORT || 80);
