697929fd4810f91eb34db27b1d05326b074f349c
Alex Luu <Alex.luu@loopio.com>
2019-10-09 21:05:04 -0400

    fixed conflict merges

--- extension/build_script.sh
+++ extension/build_script.sh
@@ -7,5 +7,5 @@ cp -r src/css/* build/css
 cp -r src/js/lib/* build/js/lib
 node html_to_string.js
 webpack-cli  --config ./webpack.config.js --watch --entry ./src/js/inject/index.js -o build/js/inject.js &
-webpack-cli  --config ./webpack.config.js --watch --entry ./src/js/background/index.js -o build/js/background.js
+webpack-cli  --config ./webpack.config.js --watch --entry ./src/js/background/index.js -o build/js/background.js &
 echo "done"
