d4e2942560fa4ea425d5cb5bac01c3f8b7e151fa
ArsalaBangash <arsalabangash@gmail.com>
2019-10-12 21:17:12 -0400

    Modified build_script and gitignore

--- extension/build_script.sh
+++ extension/build_script.sh
@@ -1,7 +1,5 @@
 rm -rf build/*
-cp src/* build
-cp -r src/css/* build/css
-cp -r src/js/lib/* build/js/lib
+cp -r src/* build
 node html_to_string.js
 webpack-cli  --config ./webpack.config.js --watch --entry ./src/js/inject/index.js -o build/js/inject.js &
 webpack-cli  --config ./webpack.config.js --watch --entry ./src/js/background/index.js -o build/js/background.js &
