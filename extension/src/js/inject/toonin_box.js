19c273927fb9b9946dcba3458e14121827ab51bf
Ali Raza <aliraza@ibm.com>
2019-10-19 17:01:12 -0400

    added css differently

--- extension/src/js/inject/toonin_box.js
+++ extension/src/js/inject/toonin_box.js
@@ -1,6 +1,5 @@
 module.exports = {html: `<div>
 <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-<link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.blue-light_blue.min.css" />
 <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.12/css/all.css" integrity="sha384-G0fIWCsCzJIMAVNQPfjH08cyYaUtMwjJwqiRKxxE/rx96Uroj1BtIQ6MLJuheaO9"
     crossorigin="anonymous">
