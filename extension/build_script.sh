bf995508acb6ae2e4892d7dcfdb4f9485173c1a9
Alex Luu <Alex.luu@loopio.com>
2019-10-20 22:36:12 -0400

    the build script was broken, this watches our new js files

--- extension/build_script.sh
+++ extension/build_script.sh
@@ -1,6 +1,6 @@
 rm -rf build/*
 cp -r src/* build
-node html_to_string.js
-webpack-cli  --config ./webpack.config.js --watch --entry ./src/js/inject/index.js -o build/js/inject.js &
-webpack-cli  --config ./webpack.config.js --watch --entry ./src/js/background/index.js -o build/js/background.js &
-echo "done"
+webpack-cli  --config ./webpack.config.js --watch --entry ./src/popup.js -o build/popup.js &
+webpack-cli  --config ./webpack.config.js --watch --entry ./src/manifest.json -o build/manifest.json &
+webpack-cli  --config ./webpack.config.js --watch --entry ./src/js/background/index.js -o build/js/background/index.js &
+echo "Watching over 3 files for changes, popup.js mainfest.json and js/background/index.js, if this is run multiple times you may want to run 'killall' node in the command line"
