5754696a45e79024c5890c018d044e93a5d4b03b
Alex Luu <Alex.luu@loopio.com>
2019-10-20 22:56:50 -0400

    added new build script to only build once and all chrome extension is updated to watch new js files

--- extension/build_script.sh
+++ extension/build_script.sh
@@ -1,6 +1,6 @@
 rm -rf build/*
 cp -r src/* build
 webpack-cli  --config ./webpack.config.js --watch --entry ./src/popup.js -o build/popup.js &
-webpack-cli  --config ./webpack.config.js --watch --entry ./src/manifest.json -o build/manifest.json &
-webpack-cli  --config ./webpack.config.js --watch --entry ./src/js/background/index.js -o build/js/background/index.js &
-echo "Watching over 3 files for changes, popup.js mainfest.json and js/background/index.js, if this is run multiple times you may want to run 'killall' node in the command line"
+webpack-cli  --config ./webpack.config.js --watch --entry ./src/js/background/index.js -o build/js/background.js &
+webpack-cli  --config ./webpack.config.js --watch --entry ./src/js/inject/index.js -o build/js/inject.js &
+echo "Watching over 3 javascript files for changes, popup.jsand js/background/index.js, if this is run multiple times you may want to run 'killall' node in the command line"
