9d1cf705f91ca7fef087f0556d51eb07bec040f0
Ali Raza <alir.raza@mail.utoronto.ca>
2019-11-10 18:20:10 -0500

    https on client

--- client/server.js
+++ client/server.js
@@ -1,12 +1,17 @@
-const express = require('express')
-const serveStatic = require('serve-static')
-const path = require('path')
-
+const express = require('express');
+const serveStatic = require('serve-static');
+const path = require('path');
+const fs = require('fs');
 const app = express();
 
 const PORT = 3000;
 
+var https = require('https').createServer({
+  key: fs.readFileSync('server.key'),
+  cert: fs.readFileSync('server.cert')
+}, app);
+
 
 app.use('/', serveStatic(path.join(__dirname, '/dist')));
 
-app.listen(PORT, () => { console.log('listening on port 3000')});
+https.listen(PORT, () => { console.log('listening on port 3000')});
