d05dd73044701aa03df62c53b86a4c5306406044
ArsalaBangash <arsalabangash@gmail.com>
2020-05-10 22:31:38 -0400

    Added AudioPlayer component, which displays when only an audio stream is available

--- packages/app/quasar.conf.js
+++ packages/app/quasar.conf.js
@@ -13,7 +13,7 @@ module.exports = function (ctx) {
     // --> boot files are part of "main.js"
     // https://quasar.dev/quasar-cli/cli-documentation/boot-files
     boot: [
-
+      'vue-audio-visual'
     ],
 
     // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-css
---
+++ packages/app/src/components/AudioPlayer.vue
@@ -0,0 +1,37 @@
+<template>
+  <audio
+    ref="player"
+    controls
+    autoplay
+  />
+</template>
+<script>
+export default {
+  props: {
+    stream: {
+      type: MediaStream,
+    },
+  },
+  data () {
+    return {
+      media: null
+    }
+  },
+  mounted () {
+    const audioStream = new MediaStream([...this.stream.getAudioTracks()])
+    this.$refs.player.srcObject = audioStream
+    this.media = audioStream
+  }
+}
+</script>
+
+<style scoped>
+audio {
+  margin: 0px 24px;
+  box-shadow: 5px 5px 20px rgba(0, 0, 0, 0.4);
+  border-radius: 90px;
+  transform: scale(1.05);
+  left: 50%;
+  top: 20%;
+}
+</style>
